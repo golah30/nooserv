18038facc1674e7b458e2e7013a9d2f828576cb0
Dmitriy Kurilenko <golah30@gmail.com>
2018-12-10 22:03:16 +0200

    fixed * response

--- app.js
+++ app.js
@@ -39,9 +39,7 @@ app.use(function(req, res, next) {
 app.use("/api", index);
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(path.join(__dirname, "./nootron/build", "index.html"))
-  );
+  res.sendFile(path.resolve(path.join(__dirname, "/public", "index.html")));
 });
 
 // catch 404 and forward to error handler
